9adb09710d9c42787077d3e0e532875cfaf3f1bf
Ashok Kumar T <ashok.bioinformatics@gmail.com>
2018-11-03 21:01:25 +0530

    Add files via upload

--- help.php
+++ help.php
@@ -1,3 +1,18 @@
-<?php
-print "Help";
-?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>locBLAST - Local NCBI BLAST+ Search</title>
+	<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<div class="output">
+		<h2 style="text-align: center;"><i style="font-size: medium">loc</i><span style="color:blue; font-size: large">BLAST</span> - <span style="font-size: large;">local NCBI BLAST</span></h2>
+		<hr class="heffect" />
+		<div style="padding: 0 15px 0 15px;">
+		<a href="index.php" title="Go Back to Home"><div style="padding: 0 5px 0 0;" id="back"></div></a>
+		<h4 style="text-align:center; color:blue" id="top">Tutorial</h4>
+		Will be updated soon.
+		</div>
+	</div>
+</body>
+</html>
