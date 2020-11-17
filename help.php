f416d41a71a8a63ec1b5c3c2170e054a521fd15b
Ashok Kumar T <ashok.bioinformatics@gmail.com>
2020-11-17 13:32:26 +0530

    Help File

--- help.php
+++ help.php
@@ -11,7 +11,20 @@
 		<div style="padding: 0 15px 0 15px;">
 		<a href="index.php" title="Go Back to Home"><div style="padding: 0 5px 0 0;" id="back"></div></a>
 		<h4 style="text-align:center; color:blue" id="top">Tutorial</h4>
-		Will be updated soon.
+		<ol>
+			<li><p>Open the homepage of the <i>loc</i>BLAST server.</p></li>
+			<li><p>Select the program <i>blastn</i>, <i>blastp</i>, <i>blastx</i>, <i>tblastn</i>, <i>tblastx</i>, <i>deltablast</i>, <i>psiblast</i>, <i>rpsblast</i>, or <i>rpstblastn</i> according to the query sequence in the <b>Program</b> section.</p></li>
+			<li><p>Select the appropriate database from the list box present in the <b>Database</b> section. The default database is <i>Non-redundant Protein/Nucleotide Sequences</i> (NR/NT).</p></li>
+			<li><p>Enter the query sequence as FASTA format in the text box (or upload FASTA format sequence file).</p></li>
+			<li><p>Select the output format for the BLAST result in the <b>Output</b> section.</p></li>
+			<li><p>Click the <i>Search</i> button or <i>Advanced Search</i> button (if <b>Algorithm Parameters</b> selected) to perform sequence similarity search.</p></li>
+		</ol>
+		<h4 style="text-align:center; color:blue" id="top">Tips</h4>
+		<ul>
+			<li><p>Move mouse over hyperlinks and icons for tooltip text description.</p></li>
+			<li><p>Click hyperlinks and icons for a brief description.</p></li>
+			<li><p>Carefully change <b>Algorithm Parameters</b> (only if needed) according to the query and BLAST program algorithm.</p></li>
+		</ul>
 		</div>
 	</div>
 </body>
