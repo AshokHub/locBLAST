aadd527e53b222267d7b966d520f36e6d6ce6706
Ashok Kumar T <ashok.bioinformatics@gmail.com>
2020-11-22 10:32:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
+	<link rel="icon" href="favicon.ico" type="image/ico" sizes="32x32">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>locBLAST - Local NCBI BLAST+ Search</title>
 	<link href="style.css" rel="stylesheet" type="text/css" />
