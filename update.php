533e5eb7a77d385766bc978d6d5a5da76a5c6e14
Ashok Kumar T <ashok.bioinformatics@gmail.com>
2020-12-01 14:22:30 +0530

    Update update.php

--- update.php
+++ update.php
@@ -1,4 +1,16 @@
 <?php
+if (strtoupper(substr(php_uname('s'), 0, 3)) === 'WIN') {
+	$osextn = ".exe";
+	$osstrt = "";
+} else {
+	$osextn = "";
+	$osstrt = "./";
+}
+$cmd = $osstrt . "blastdbcmd" . $osextn . " -version";
+exec($cmd, $output);
+$old_version = $output[0];
+$old_v = preg_replace("/[^.0-9]+/", "", $old_version);
+
 $host = 'ftp.ncbi.nlm.nih.gov';
 $user = 'anonymous';
 $password = 'anonymous';
@@ -11,9 +23,20 @@ if ((!$ftpConn) || (!$login)) {
 }
 ftp_chdir($ftpConn, $blast_exe_dir);
 $dir = ftp_nlist($ftpConn, '');
-$src = $dir[5];
-$version = ltrim($src, 'ncbi-blast-');
-$version = rtrim($version, '-src.tar.gz');
+$n = count($dir);
+for ($i = 0; $i < $n; $i++) {
+	if (preg_match("/src.tar.gz$/i", $dir[$i]) == 1) {
+		$new_src = $dir[$i];
+	}
+}
+$new_version = ltrim($new_src, 'ncbi-blast-');
+$new_version = rtrim($new_version, '-src.tar.gz');
 ftp_close($ftpConn);
-echo "<b>NEW:</b> Blast v$version";
+$new_v = preg_replace("/[^.0-9]+/", "", $new_version);
+
+if ($new_v > $old_v) {
+	echo "<b>NEW:</b> <a href='ftp://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST' title='Download'>Blast v$new_version</a>";
+} else {
+	echo "You are running the latest version!";
+}
 ?>
