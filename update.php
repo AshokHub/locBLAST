c7ca572ca568f80a9546882f34e9bb2e72d28d2c
Ashok Kumar T. <ashok.bioinformatics@gmail.com>
2026-01-16 23:09:03 +0530

    Refactor version check to use HTML scraping

--- update.php
+++ update.php
@@ -11,32 +11,15 @@ exec($cmd, $output);
 $old_version = $output[0];
 $old_v = preg_replace("/[^.0-9]+/", "", $old_version);
 
-$host = 'ftp.ncbi.nlm.nih.gov';
-$user = 'anonymous';
-$password = 'anonymous';
-$ftpConn = ftp_connect($host);
-$login = ftp_login($ftpConn, $user, $password);
-$blast_exe_dir = "/blast/executables/LATEST";
-if ((!$ftpConn) || (!$login)) {
-	ftp_close($ftpConn);
-	echo 'NA';
-}
-ftp_chdir($ftpConn, $blast_exe_dir);
-$dir = ftp_nlist($ftpConn, '');
-$n = count($dir);
-for ($i = 0; $i < $n; $i++) {
-	if (preg_match("/src.tar.gz$/i", $dir[$i]) == 1) {
-		$new_src = $dir[$i];
-	}
-}
-$new_version = ltrim($new_src, 'ncbi-blast-');
-$new_version = rtrim($new_version, '-src.tar.gz');
-ftp_close($ftpConn);
-$new_v = preg_replace("/[^.0-9]+/", "", $new_version);
+$html = file_get_contents("https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/");
+	
+preg_match('/<a href=[^>]*>ncbi-blast-(.*?)(\+.*)<\/a>/is', $html, $match); 
+
+$new_v = $match[1];
 
 if ($new_v > $old_v) {
-	echo "<b>NEW:</b> <a href='ftp://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST' title='Download'>Blast v$new_version</a>";
+	echo "<b>NEW:</b> <a href='ftp://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST' title='Download'>BLAST v" . $match[1] . "+</a>";
 } else {
-	echo "You are running the latest version!";
+	echo "Latest Version!";
 }
 ?>
